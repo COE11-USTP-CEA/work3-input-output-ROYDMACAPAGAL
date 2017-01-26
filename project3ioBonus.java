db38031097dee3d3571e243f3de3cee67c20b6e5
Royd G. Macapagal <ryoumacapagal@gmail.com>
2017-01-26 23:37:21 +0800

    Add files via upload

---
+++ project3ioBonus.java
@@ -0,0 +1,14 @@
+class project3ioBonus{
+    public static void main ( String [] args ){
+        for( int i=1; i<10; i+=2 ){
+            for( int k=0; k<(4-i/2); k++ )
+            {
+                System.out.print(" ");
+            }
+            for( int j=0; j<i; j++ ){
+                System.out.print("R");
+            }
+            System.out.println("");
+        }
+    }
+}
